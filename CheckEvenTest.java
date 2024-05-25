5105e4f4bb0a8708ee371745dff13420ac21d7c7
AlsuFav <143123118+AlsuFav@users.noreply.github.com>
2024-05-25 13:59:40 +0300

    Update CheckEvenTest.java

--- CheckEvenTest.java
+++ CheckEvenTest.java
@@ -1,14 +1,1 @@
-package test2;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-public class CheckEvenTest {
-
-    @Test
-    public void testCheckEven() {
-        assertEquals(0, checkEven(new byte[0]));
-        assertEquals(1, checkEven(new byte[]{1, 2, 3, 4}));
-        assertEquals(1, checkEven(new byte[]{1, 2, 3, 4, 5, 6}));
-    }
