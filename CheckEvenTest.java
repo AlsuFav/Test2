f06cd38aa1a6efb4009b5e9f4b05f9f0893dbe95
AlsuFav <143123118+AlsuFav@users.noreply.github.com>
2024-05-25 13:58:43 +0300

    Update CheckEvenTest.java

--- CheckEvenTest.java
+++ CheckEvenTest.java
@@ -12,21 +12,3 @@ public class CheckEvenTest {
         assertEquals(1, checkEven(new byte[]{1, 2, 3, 4}));
         assertEquals(1, checkEven(new byte[]{1, 2, 3, 4, 5, 6}));
     }
-
-    private int checkEven(byte[] data) {
-        int even = 0;
-        for (byte b : data) {
-            int count = 0;
-            for (int i = 0; i < 8; i++) {
-                count += (b >> i & 1);
-            }
-
-            if (count % 2 == 0) {
-                even += 0;
-            } else {
-                even += 1;
-            }
-        }
-        return even % 2;
-    }
-}
